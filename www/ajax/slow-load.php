ae0366abe00d2b3bc0ca646a262be1a0b9b316d0
Mike Knoop <mike@knoopgroup.org>
2011-06-10 13:10:29 -0500

    Variable conflic when using multiple lightboxes on the same page.

--- www/ajax/slow-load.php
+++ www/ajax/slow-load.php
@@ -6,10 +6,10 @@ $uiLightboxId = date("U");
 ?>
 <div id="uiLightbox-<?php echo $uiLightboxId; ?>" class="content" style="padding: 10px; font-size: 28px;">
   It works!
-  <a href="#" class="uiButton uiButtonNormal uiButtonLarge" style="float: right;" onclick="uiLightbox.toggleLightbox('hide'); return false;">Close</a>
+  <a href="#" class="uiButton uiButtonNormal uiButtonLarge" style="float: right;" onclick="uiLightbox_<?php echo $uiLightboxId; ?>.toggleLightbox('hide'); return false;">Close</a>
 </div>
 
   <script type="text/javascript">
     // Get a reference to the uiLightbox which ajax loaded this script
-    var uiLightbox = $('#uiLightbox-<?php echo $uiLightboxId; ?>').parent().parent().data('uiLightbox');   
+    var uiLightbox_<?php echo $uiLightboxId; ?> = $('#uiLightbox-<?php echo $uiLightboxId; ?>').parent().parent().data('uiLightbox');   
   </script>
